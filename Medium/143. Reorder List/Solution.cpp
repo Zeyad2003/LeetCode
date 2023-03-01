fd2d89a834f059fd8d21eda6e1709b3121887634
Zeyad2003 <zeyadnasef85@gmail.com>
2023-03-01 17:35:58 +0200

    Add Medium problem

---
+++ Medium/143. Reorder List/Solution.cpp
@@ -0,0 +1,55 @@
+class Solution {
+private:
+    ListNode *tmp = new ListNode(0); // tmp pointer to be used when it's need to save specific pointer
+public:
+    int sz = 0; // to get the size of the List
+
+    void dfs(ListNode *head) {
+        if(!head) return;
+        sz++; // we have one more element
+        dfs(head->next); // go to the next
+        tmp->val = head->val; // put the reversed value
+        tmp->next = new ListNode(0);
+        tmp = tmp->next;
+    }
+
+    // this function is used to copy head list to cur
+    void fill(ListNode *head, ListNode *copy) {
+        tmp = head; // use tmp to avoid losing head pointer
+        while(tmp) { // copy all values
+            copy->val = tmp->val;
+            copy->next = new ListNode();
+            copy = copy->next;
+            tmp = tmp->next;
+        }
+    }
+
+    void reorderList(ListNode *head) {
+        if (!head->next) return; // there's just one element no need to do anything
+
+        auto * reverse = tmp; // put the first pointer to tmp then fill new list with reversed values
+        dfs(head); // fill the reversed list with reversed values
+
+        auto * copiedList = new ListNode();
+        fill(head, copiedList); // copy the head into copiedList list
+
+        int mid = sz / 2; // loop until the middle point of the list
+
+        // update the head list
+        while(mid--) {
+            head->val = copiedList->val;
+            head = head->next;
+            copiedList = copiedList->next;
+            head->val = reverse->val;
+            head = head->next;
+            reverse = reverse->next;
+        }
+
+        if(sz & 1) head->val = copiedList->val; // if the size is odd -> the middle element should be added manually
+
+        // delete all other pointers to avoid memory leaks
+        delete reverse, reverse = nullptr;
+        delete copiedList, copiedList = nullptr;
+        delete tmp, tmp = nullptr;
+    }
+};
