9931f09b0aa29b115880fef0a7ee26663020f939
Zeyad2003 <zeyadnasef85@gmail.com>
2023-04-09 17:14:58 +0200

    9-April LeetCode Daily Challenge

--- Hard/1857. Largest Color Value in a Directed Graph/Solution.cpp
+++ Hard/1857. Largest Color Value in a Directed Graph/Solution.cpp
@@ -1,63 +1,66 @@
 class Solution {
 public:
-    bool cycle = false; // used to detect if we have a cycle in the graph
+    vector<vector<int> > adj; // To build the graph
+    vector<bool> vis; // To mark the visited nodes
+    vector<bool> inStack; // To detect if there's a cycle in the graph
 
-    void dfs(vector<vector<int> > &adj, vector<bool> &visited, vector<bool> &recStack, int node) {
-        visited[node] = true;
-        recStack[node] = true;
+    void dfs(int node, bool &cycle) {
+        vis[node] = true; // Mark the node as visited
+        inStack[node] = true; // Put it in the stack
 
-        for (int child: adj[node]) {
-            if (!visited[child])
-                dfs(adj, visited, recStack, child);
-            else if (recStack[child]) return cycle = true, void();
+        for (auto & child: adj[node]) { // Go to the neighbors of the current node
+            if (!vis[child])
+                dfs(child, cycle);
+            else if (inStack[child]) return cycle = true, void(); // Found in the stack -> There's a cycle
         }
 
-        recStack[node] = false;
+        inStack[node] = false; // Remove it from the stack
     }
 
-
-
-    int largestPathValue(string colors, vector<vector<int>> &edges) {
+    int largestPathValue(string colors, vector<vector<int>>& edges) {
         int n = (int) colors.size();
-        vector<vector<int> > adj(n);
-        vector<int> inDegree(n, 0); // used to calculate the in-degree of each node when we build the graph
+        vector<int> inDegree(n, 0); // Used to indicate the number of inner edges to every node
+        queue<int> ready; // To process the ready nodes level by level -> <node, maxColor>
+        int freq[n][26]; // Store the maximum color frequency of each node
+        memset(freq, 0, sizeof freq); // Initialize the freq array with 0
+        adj = vector<vector<int> > (n);
+        vis = vector<bool> (n, false);
+        inStack = vector<bool> (n, false);
 
-        for (auto &it: edges) { // build the graph
-            auto[u, v] = tie(it[0], it[1]);
+        for(auto & i : edges) { // Build the graph
+            auto [u, v] = tie(i[0], i[1]);
             adj[u].emplace_back(v);
-            inDegree[v]++;
+            inDegree[v]++; // Increase the indegree of the vertex
         }
 
-        vector<bool> visited(n, false); // used to mark the visited nodes
-        vector<bool> recStack(n, false); // used to detect if we have a cycle in the graph
-        for (int node = 0; node < n; node++)
-            if (!visited[node])
-                dfs(adj, visited, recStack, node); // run dfs and check if the graph has a cycle
+        bool cycle = false; // Variable to detect the cycle is found in the graph
+        for (int node = 0; node < n; ++node) { // Loop through every node in graph
+            if(!inDegree[node]) ready.emplace(node); // Put the ready node in the queue
+            if(!vis[node]) dfs(node, cycle);
+        }
 
-        if (cycle) return -1; // if the graph has a cycle -> return -1
+        if(cycle) return -1; // Found cycle -> Return -1
 
-        queue<int> ready;
-        for (int node = 0; node < n; node++) // put the nodes with in-degree = 0 in the queue to apply top-sort
-            if (!inDegree[node])
-                ready.emplace(node);
-
-        vector<vector<int> > store(n, vector<int> (26, 0)); // store[i][j] = the number of nodes with color j in the path from node 0 to node i
-        while(!ready.empty()) {
-            auto curNode = ready.front(); // get the current node
+        while(!ready.empty()) { // Start processing the ready nodes
+            auto curNode = ready.front(); // Get the current ready node
             ready.pop(); // remove it from the queue
-            store[curNode][colors[curNode] - 'a']++; // increase the number of nodes with color colors[curNode] in the path from node 0 to node curNode
 
-            for(auto & child : adj[curNode]) { // loop on the children of the current node
-                for(int i = 0; i < 26; i++) // loop on all colors
-                    store[child][i] = max(store[child][i], store[curNode][i]); // update the number of nodes with color i in the path from node 0 to node child
-                if(!--inDegree[child]) // if the in-degree of the child becomes 0 -> put it in the queue
+            freq[curNode][colors[curNode] - 'a']++; // Increase its color
+            for(auto &child : adj[curNode]) { // Loop on its children
+                for(int i = 0; i < 26; i++) // Pass the max frequency for every color to the child
+                    freq[child][i] = max(freq[child][i], freq[curNode][i]);
+
+                if(!--inDegree[child]) // Remove the edge [curNode -> child] and if it becomes ready put it in the queue
                     ready.emplace(child);
             }
         }
 
-        int ret = 0;
-        for(int i = 0; i < n; i++) // loop on all nodes and get the color that appears the most in the path from node 0 to node i
-            ret = max(ret, *max_element(store[i].begin(), store[i].end()));
-        return ret; // return the answer
+        int ans = 0;
+        for (int node = 0; node < n; ++node) {
+            for (int color = 0; color < 26; ++color) {
+                ans = max(ans, freq[node][color]); // Loop through all colors for all nodes and maximize the answer
+            }
+        }
+        return ans; // Return the maximum answer
     }
 };
