e1e76d06e0fb077c5ee6950ef94d63b25a3ec871
Zeyad2003 <zeyadnasef85@gmail.com>
2023-04-15 10:14:10 +0200

    15-April LeetCode Daily Challenge

---
+++ Hard/2218. Maximum Value of K Coins From Piles/Solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int dp[2005][1005]; // dp[i][j] -> The maximum value of coins we can get from piles[i..n - 1] if we take j coins
+    int n; // The size of the piles vector
+
+    int solve(vector<vector<int> > &piles, int k, int i = 0) {
+        if(i >= n) return 0; // No more piles
+
+        auto & ret = dp[k][i];
+        if(~ret) return ret; // Calculate before -> return it
+
+        ret = 0; // Start with a minimum value
+
+        for (int j = 0; j <= min(k, (int) piles[i].size() - 1); ++j) { // For every pile, try to take [0:min(k, piles[i].size())] coins
+            int val = piles[i][0] - piles[i][j]; // The summation of the coins we take
+            ret = max(ret, solve(piles, k - j, i + 1) + val); // Try to take the maximum value
+        }
+
+        return ret; // Return your maximum value
+    }
+
+    int maxValueOfCoins(vector<vector<int>>& piles, int k) {
+        n = (int) piles.size(); // Initialize the size of the piles vector
+        memset(dp, -1, sizeof dp); // Initialize the dp array with -1
+
+        for(int i = 0; i < n; i++) { // Prepare the prefix sum array of the piles vector
+            piles[i].emplace_back(0); // Add a 0 at the end of each pile
+            for (int j = (int) piles[i].size() - 2; j >= 0; --j) // Apply the prefix sum from the end to the beginning
+                piles[i][j] += piles[i][j + 1];
+        }
+
+        return solve(piles, k); // Return the optimal solution
+    }
+};
